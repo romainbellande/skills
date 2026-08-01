0ab3107e0efcaad2ed2638e8f278f53d156494a7
Romain Bellande <rombelsecure@gmail.com>
2026-08-01 16:16:41 +0200

    chore: replace changesets with semantic-release

---
+++ .releaserc.mjs
@@ -0,0 +1,16 @@
+export default {
+  branches: ['main'],
+  plugins: [
+    ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
+    ['@semantic-release/release-notes-generator', { preset: 'conventionalcommits' }],
+    ['@semantic-release/npm', { npmPublish: false }],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
